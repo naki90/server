35581eb93360358638bf8036645a7feb797c0d74
naki90 <143862130+naki90@users.noreply.github.com>
2023-09-09 02:40:27 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install openjdk-17-jre-headless
+sudo apt install screen
+sudo apt install ufw
+sudo ufw allow 25565
+wget https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
